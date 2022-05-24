c78c3a69a6f468ee89873a96a3a30569872393a0
timipani <164621408+timipani@users.noreply.github.com>
2022-05-24 21:52:49 +0000

    Update serveredit.php

--- ai_economist/foundation/base/base_agent.py
+++ ai_economist/foundation/base/base_agent.py
@@ -20,4 +20,39 @@ class BaseAgent:
     construction (see BaseComponent.get_additional_state_fields in base_component.py).
 
     They also provide a simple API for getting/setting actions for each of their
-    registered action subspaces (whic
+    registered action subspaces (which depend on the components used to build
+    the environment).
+
+    Args:
+        idx (int or str): Index that uniquely identifies the agent object amongst the
+            other agent objects registered in its environment.
+        multi_action_mode (bool): Whether to allow the agent to take one action for
+            each of its registered action subspaces each timestep (if True),
+            or to limit the agent to take only one action each timestep (if False).
+    """
+
+    name = ""
+
+    def __init__(self, idx=None, multi_action_mode=None):
+        assert self.name
+
+        if idx is None:
+            idx = 0
+
+        if multi_action_mode is None:
+            multi_action_mode = False
+
+        if isinstance(idx, str):
+            self._idx = idx
+        else:
+            self._idx = int(idx)
+
+        self.multi_action_mode = bool(multi_action_mode)
+        self.single_action_map = (
+            {}
+        )  # Used to convert single-action-mode actions to the general format
+
+        self.action = dict()
+        self.action_dim = dict()
+        self._action_names = []
+        self
