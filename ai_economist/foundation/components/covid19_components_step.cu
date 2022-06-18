962852303d3db80939b8fcb6c450be8ecf77daa6
timipani <164621408+timipani@users.noreply.github.com>
2022-06-18 21:52:48 +0000

    commit fix

--- ai_economist/foundation/components/covid19_components_step.cu
+++ ai_economist/foundation/components/covid19_components_step.cu
@@ -13,4 +13,34 @@ extern "C" {
         int * action_in_cooldown_until,
         const int * kDefaultAgentActionMask,
         const int * kNoOpAgentActionMask,
-        const int kNumStringencyLeve
+        const int kNumStringencyLevels,
+        int * actions,
+        float * obs_a_stringency_policy_indicators,
+        float * obs_a_action_mask,
+        float * obs_p_stringency_policy_indicators,
+        int * env_timestep_arr,
+        const int kNumAgents,
+        const int kEpisodeLength
+    ) {
+        const int kEnvId = blockIdx.x;
+        const int kAgentId = threadIdx.x;
+
+        // Increment time ONCE -- only 1 thread can do this.
+        if (kAgentId == 0) {
+            env_timestep_arr[kEnvId] += 1;
+        }
+
+        // Wait here until timestep has been updated
+        __syncthreads();
+
+        assert(env_timestep_arr[kEnvId] > 0 &&
+            env_timestep_arr[kEnvId] <= kEpisodeLength);
+        assert (kAgentId <= kNumAgents - 1);
+
+        // Update the stringency levels for the US states
+        if (kAgentId < (kNumAgents - 1)) {
+            // Indices for time-dependent and time-independent arrays
+            // Time dependent arrays have shapes
+            // (num_envs, kEpisodeLength + 1, kNumAgents - 1)
+            // Time independent arrays have shapes (num_envs, kNumAgents - 1)
+            const int
