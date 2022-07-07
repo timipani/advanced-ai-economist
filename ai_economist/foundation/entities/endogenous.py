75bc789677e8ad175d231e7959f2ffc1a834d229
timipani <164621408+timipani@users.noreply.github.com>
2022-07-07 17:30:12 +0000

    update scheme

--- ai_economist/foundation/entities/endogenous.py
+++ ai_economist/foundation/entities/endogenous.py
@@ -10,4 +10,27 @@ from ai_economist.foundation.base.registrar import Registry
 class Endogenous:
     """Base class for endogenous entity classes.
 
-    Endogenous entities are those that, conceptually, describe the
+    Endogenous entities are those that, conceptually, describe the internal state
+    of an agent. This provides a convenient way to separate physical entities (which
+    may exist in the world, be exchanged among agents, or are otherwise in principal
+    observable by others) from endogenous entities (such as the amount of labor
+    effort an agent has experienced).
+
+    Endogenous entities are registered in the "endogenous" portion of an agent's
+    state and should only be observable by the agent itself.
+    """
+
+    name = None
+
+    def __init__(self):
+        assert self.name is not None
+
+
+endogenous_registry = Registry(Endogenous)
+
+
+@endogenous_registry.add
+class Labor(Endogenous):
+    """Labor accumulated through working. Included in all environments by default."""
+
+    name = "Labor"
