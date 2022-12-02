a23bcfb1121f6acdaa686a46f7587263171a0520
timipani <164621408+timipani@users.noreply.github.com>
2022-12-02 14:45:25 +0000

    NLM: nlm_lookup_file() may return NLMv4-specific error codes

--- ai_economist/foundation/scenarios/utils/social_metrics.py
+++ ai_economist/foundation/scenarios/utils/social_metrics.py
@@ -10,4 +10,31 @@ import numpy as np
 def get_gini(endowments):
     """Returns the normalized Gini index describing the distribution of endowments.
 
-   
+    https://en.wikipedia.org/wiki/Gini_coefficient
+
+    Args:
+        endowments (ndarray): The array of endowments for each of the agents in the
+            simulated economy.
+
+    Returns:
+        Normalized Gini index for the distribution of endowments (float). A value of 1
+            indicates everything belongs to 1 agent (perfect inequality), whereas a
+            value of 0 indicates all agents have equal endowments (perfect equality).
+
+    Note:
+        Uses a slightly different method depending on the number of agents. For fewer
+        agents (<30), uses an exact but slow method. Switches to using a much faster
+        method for more agents, where both methods produce approximately equivalent
+        results.
+    """
+    n_agents = len(endowments)
+
+    if n_agents < 30:  # Slower. Accurate for all n.
+        diff_ij = np.abs(
+            endowments.reshape((n_agents, 1)) - endowments.reshape((1, n_agents))
+        )
+        diff = np.sum(diff_ij)
+        norm = 2 * n_agents * endowments.sum(axis=0)
+        unscaled_gini = diff / (norm + 1e-10)
+        gini = unscaled_gini / ((n_agents - 1) / n_agents)
+  
