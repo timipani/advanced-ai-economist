dce57a8d970497579739b27aa14f1fdc87f414f7
timipani <164621408+timipani@users.noreply.github.com>
2022-12-18 05:31:21 +0000

    firewall3: update to git head

--- ai_economist/real_business_cycle/rbc/constants.py
+++ ai_economist/real_business_cycle/rbc/constants.py
@@ -25,4 +25,35 @@ def all_agents_export_experiment_template(
     consumption_choices = np.array(
         list(itertools.product(*consumption_choices)), dtype=_NP_DTYPE
     )
-    w
+    work_choices = np.array(list(itertools.product(*work_choices)), dtype=_NP_DTYPE)
+
+    price_choices = np.array([0.0 + 500.0 * c for c in range(6)], dtype=_NP_DTYPE)
+    wage_choices = np.array([0.0, 11.0, 22.0, 33.0, 44.0], dtype=_NP_DTYPE)
+    capital_choices = np.array([0.1], dtype=_NP_DTYPE)
+    price_and_wage = np.array(
+        list(itertools.product(price_choices, wage_choices, capital_choices)),
+        dtype=_NP_DTYPE,
+    )
+
+    # government action discretization
+    income_taxation_choices = np.array(
+        [0.0 + 0.2 * c for c in range(6)], dtype=_NP_DTYPE
+    )
+    corporate_taxation_choices = np.array(
+        [0.0 + 0.2 * c for c in range(6)], dtype=_NP_DTYPE
+    )
+    tax_choices = np.array(
+        list(itertools.product(income_taxation_choices, corporate_taxation_choices)),
+        dtype=_NP_DTYPE,
+    )
+    global_state_dim = (
+        NUMFIRMS  # prices
+        + NUMFIRMS  # wages
+        + NUMFIRMS  # stocks
+        + NUMFIRMS  # was good overdemanded
+        + 2 * NUMGOVERNMENTS  # tax rates
+        + 1
+    )  # time
+
+    global_state_digit_dims = list(
+  
