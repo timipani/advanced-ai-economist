82a1b942f7179d5b117473432338f158bb22cd2a
timipani <164621408+timipani@users.noreply.github.com>
2022-12-22 09:31:41 +0000

    New Tests

--- ai_economist/real_business_cycle/rbc/constants.py
+++ ai_economist/real_business_cycle/rbc/constants.py
@@ -224,4 +224,43 @@ def all_agents_short_export_experiment_template(
     wage_choices = np.array([0.0, 11.0, 22.0, 33.0, 44.0], dtype=_NP_DTYPE)
     capital_choices = np.array([0.1], dtype=_NP_DTYPE)
     price_and_wage = np.array(
-        list(itertools.product(price_choices, wage_choices, capital_choices)
+        list(itertools.product(price_choices, wage_choices, capital_choices)),
+        dtype=_NP_DTYPE,
+    )
+
+    # government action discretization
+    income_taxation_choices = np.array(
+        [0.0 + 0.2 * c for c in range(6)], dtype=_NP_DTYPE
+    )
+    corporate_taxation_choices = np.array(
+        [0.0 + 0.2 * c for c in range(6)], dtype=_NP_DTYPE
+    )
+    tax_choices = np.array(
+        list(itertools.product(income_taxation_choices, corporate_taxation_choices)),
+        dtype=_NP_DTYPE,
+    )
+    global_state_dim = (
+        NUMFIRMS  # prices
+        + NUMFIRMS  # wages
+        + NUMFIRMS  # stocks
+        + NUMFIRMS  # was good overdemanded
+        + 2 * NUMGOVERNMENTS  # tax rates
+        + 1
+    )  # time
+
+    global_state_digit_dims = list(
+        range(2 * NUMFIRMS, 3 * NUMFIRMS)
+    )  # stocks are the only global state var that can get huge
+    consumer_state_dim = (
+        global_state_dim + 1 + 1
+    )  # budget  # theta, the disutility of work
+
+    firm_state_dim = (
+        global_state_dim
+        + 1  # budget
+        + 1  # capital
+        + 1  # production alpha
+        + NUMFIRMS  # onehot specifying which firm
+    )
+
+    episodes_to_anneal_firm
