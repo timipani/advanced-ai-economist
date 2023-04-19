8daa73400020fd635175835fb60a0e3db626b6aa
timipani <164621408+timipani@users.noreply.github.com>
2023-04-19 21:48:02 +0000

    powerpc: Make {cmp}xchg* and their atomic_ versions fully ordered

--- ai_economist/real_business_cycle/train_bestresponse.py
+++ ai_economist/real_business_cycle/train_bestresponse.py
@@ -22,3 +22,38 @@ def check_if_ep_str_policy_exists(rollout_path, ep_str):
 
 def run_rollout(rollout_path, arguments):
     """
+    # take in rollout directory
+    # load latest policies and the action functions and the hparams dict
+    # make a cudamanager obj and run the job
+    # this will require initializing everything as before, resetting,
+    # and running naive policy gradient training at some fixed learning rate
+    """
+    with open(rollout_path / Path("action_arrays.pickle"), "rb") as f:
+        action_arrays = pickle.load(f)
+
+    consumption_choices, work_choices, price_and_wage, tax_choices = (
+        action_arrays["consumption_choices"],
+        action_arrays["work_choices"],
+        action_arrays["price_and_wage"],
+        action_arrays["tax_choices"],
+    )
+
+    cfg_dict = cfg_dict_from_yaml(
+        rollout_path / Path("hparams.yaml"),
+        consumption_choices,
+        work_choices,
+        price_and_wage,
+        tax_choices,
+    )
+
+    print(cfg_dict)
+
+    if arguments.agent_type == "all":
+        agent_types = ["consumer", "firm", "government"]
+    else:
+        agent_types = [arguments.agent_type]
+
+    for agent_type in agent_types:
+        ep_rewards = defaultdict(list)
+        for _ in range(arguments.repeat_runs):
+            for ep_st
