3cbfc6a4539fe64d6f9a1c3e2470052b4c528603
timipani <164621408+timipani@users.noreply.github.com>
2023-04-28 21:11:55 +0000

    Resetting to HEAD

--- ai_economist/real_business_cycle/train_multi_exps.py
+++ ai_economist/real_business_cycle/train_multi_exps.py
@@ -42,4 +42,38 @@ world_param_sweeps = {
     "interest_rate": [0.0],
     "importer_price": [500.0],
     "importer_quantity": [100.0],
-    "use_impo
+    "use_importer": [1],
+}
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dry-run", action="store_true")
+    parser.add_argument("--experiment-dir", type=str, default="experiment/experiment")
+    parser.add_argument("--group-name", type=str, default="default_group")
+    parser.add_argument("--job-name-base", type=str, default="rollout")
+    parser.add_argument("--num-consumers", type=int, default=100)
+    parser.add_argument("--num-firms", type=int, default=10)
+    parser.add_argument("--num-governments", type=int, default=1)
+    parser.add_argument("--run-only", action="store_true")
+    parser.add_argument("--seed-from-timestamp", action="store_true")
+
+    args = parser.parse_args()
+
+    (
+        default_cfg_dict,
+        consumption_choices,
+        work_choices,
+        price_and_wage,
+        tax_choices,
+        default_firm_action,
+        default_government_action,
+    ) = all_agents_short_export_experiment_template(
+        args.num_firms, args.num_consumers, args.num_governments
+    )
+
+    if args.run_only:
+        print("Not sweeping over hyperparameter combos...")
+    else:
+        for new_cfg in sweep_cfg_generator(
+ 
