b262b567ce7988b298d92154340b7f740c95b927
timipani <164621408+timipani@users.noreply.github.com>
2023-05-16 21:43:43 +0000

    v1.4.1

--- tests/run_covid19_cpu_gpu_consistency_checks.py
+++ tests/run_covid19_cpu_gpu_consistency_checks.py
@@ -22,4 +22,36 @@ try:
 except ModuleNotFoundError:
     raise ModuleNotFoundError(
         "The env consistency checker requires the 'WarpDrive' package, please run "
-        "'pip i
+        "'pip install rl-warp-drive' first."
+    ) from None
+except ValueError:
+    raise ValueError("The env consistency checker needs a GPU to run!") from None
+
+import os
+
+from ai_economist.foundation.env_wrapper import FoundationEnvWrapper
+from ai_economist.foundation.scenarios.covid19.covid19_env import (
+    CovidAndEconomyEnvironment,
+)
+
+env_registrar = EnvironmentRegistrar()
+this_file_dir = os.path.dirname(os.path.abspath(__file__))
+env_registrar.add_cuda_env_src_path(
+    CovidAndEconomyEnvironment.name,
+    os.path.join(this_file_dir, "../ai_economist/foundation/scenarios/covid19/covid19_build.cu")
+)
+env_configs = {
+    "test1": {
+        "collate_agent_step_and_reset_data": True,
+        "components": [
+            {"ControlUSStateOpenCloseStatus": {"action_cooldown_period": 28}},
+            {
+                "FederalGovernmentSubsidy": {
+                    "num_subsidy_levels": 20,
+                    "subsidy_interval": 90,
+                    "max_annual_subsidy_per_person": 20000,
+                }
+            },
+            {
+                "VaccinationCampaign": {
+             
