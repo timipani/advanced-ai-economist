a356c6d0efd75cf912b678fe7ae41829ce454b57
timipani <164621408+timipani@users.noreply.github.com>
2023-05-17 22:56:52 +0000

    miner fix

--- tests/run_covid19_cpu_gpu_consistency_checks.py
+++ tests/run_covid19_cpu_gpu_consistency_checks.py
@@ -54,4 +54,40 @@ env_configs = {
             },
             {
                 "VaccinationCampaign": {
-             
+                    "daily_vaccines_per_million_people": 3000,
+                    "delivery_interval": 1,
+                    "vaccine_delivery_start_date": "2021-01-12",
+                }
+            },
+        ],
+        "economic_reward_crra_eta": 2,
+        "episode_length": 540,
+        "flatten_masks": True,
+        "flatten_observations": False,
+        "health_priority_scaling_agents": 0.3,
+        "health_priority_scaling_planner": 0.45,
+        "infection_too_sick_to_work_rate": 0.1,
+        "multi_action_mode_agents": False,
+        "multi_action_mode_planner": False,
+        "n_agents": 51,
+        "path_to_data_and_fitted_params": "",
+        "pop_between_age_18_65": 0.6,
+        "risk_free_interest_rate": 0.03,
+        "world_size": [1, 1],
+        "start_date": "2020-03-22",
+        "use_real_world_data": False,
+        "use_real_world_policies": False,
+    }
+}
+
+num_agents = env_configs["test1"]["n_agents"]
+policy_to_agent_ids_mapping = {
+    "a": [str(agent_id) for agent_id in range(num_agents)],
+    "p": ["p"],
+}
+
+testing_class = EnvironmentCPUvsGPU(
+    dual_mode_env_class=CovidAndEconomyEnvironment,
+    env_configs=env_configs,
+    num_envs=3,
+    num_epi
