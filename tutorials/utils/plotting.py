6197f8595e0aa86a524a17015cad1bac3d3e3fbc
timipani <164621408+timipani@users.noreply.github.com>
2024-04-06 20:02:41 +0000

    heart

--- tutorials/utils/plotting.py
+++ tutorials/utils/plotting.py
@@ -1,1 +1,36 @@
-# Copy
+# Copyright (c) 2020, salesforce.com, inc.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# For full license text, see the LICENSE file in the repo root
+# or https://opensource.org/licenses/BSD-3-Clause
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from ai_economist.foundation import landmarks, resources
+
+
+def plot_map(maps, locs, ax=None, cmap_order=None):
+    world_size = np.array(maps.get("Wood")).shape
+    max_health = {"Wood": 1, "Stone": 1, "House": 1}
+    n_agents = len(locs)
+
+    if ax is None:
+        _, ax = plt.subplots(1, 1, figsize=(10, 10))
+    else:
+        ax.cla()
+    tmp = np.zeros((3, world_size[0], world_size[1]))
+    cmap = plt.get_cmap("jet", n_agents)
+
+    if cmap_order is None:
+        cmap_order = list(range(n_agents))
+    else:
+        cmap_order = list(cmap_order)
+        assert len(cmap_order) == n_agents
+
+    scenario_entities = [k for k in maps.keys() if "source" not in k.lower()]
+    for entity in scenario_entities:
+        if entity == "House":
+            continue
+        elif resources.has(entity):
+            if resou
